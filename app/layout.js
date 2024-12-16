1687b5ffe36e6dfae63262343196d01995c570da
NuthX <huermos@gmail.com>
2024-12-16 15:40:23 +0800

    feat(ui): update layout with NavBar component

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import { NavBar } from "@/components/nav-bar";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -22,6 +23,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <NavBar />
         {children}
       </body>
     </html>
