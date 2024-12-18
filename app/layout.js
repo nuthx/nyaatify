2aee991c1596ab6cb401a1b27401e33c2e1d8dde
NuthX <huermos@gmail.com>
2024-12-18 22:15:45 +0800

    feat: add logs for database js

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,12 @@
 import "./globals.css";
 import { NavBar } from "@/components/nav-bar";
 import { rssSchedule } from "@/lib/schedule";
+import { log } from "@/lib/log";
 
 // Start RSS auto update task
 // Only start in production
 if (process.env.NODE_ENV === "production") {
-  rssSchedule().catch(console.error);
+  rssSchedule().catch(log.error);
 }
 
 export default function RootLayout({ children }) {
