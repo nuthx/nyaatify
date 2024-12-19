d300e1b99491f0e1bd74b2b1ca16a76a783c1346
NuthX <huermos@gmail.com>
2024-12-19 18:10:01 +0800

    style: redesign navbar

--- components/nav-bar.jsx
+++ components/nav-bar.jsx
@@ -1,48 +1,22 @@
 import Link from "next/link";
-import { Input } from "@/components/ui/input";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export function NavBar() {
   return (
-    <div className="border-b bg-background">
-      <div className="flex h-16 items-center container mx-auto">
-        <div className="flex items-center space-x-6 mr-4">
-          <Link href="/" className="font-bold text-xl">
-            Nyaaitfy
-          </Link>
-          <nav className="flex items-center space-x-4">
-            <Link
-              href="/"
-              className="text-sm font-medium transition-colors hover:text-primary"
-            >
-              Overview
-            </Link>
-            <Link
-              href="/server"
-              className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
-            >
-              Server
-            </Link>
-            <Link
-              href="/settings"
-              className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
-            >
-              Settings
-            </Link>
-          </nav>
+    <nav className="border-b bg-background">
+      <div className="container mx-auto max-w-screen-xl flex h-16 items-center justify-between">
+        <div className="flex items-center space-x-6">
+          <Link href="/" className="font-bold text-xl mr-4">Nyaaitfy</Link>
+          <Link href="/" className="text-sm font-medium transition-colors hover:text-primary">Home</Link>
+          <Link href="/server" className="text-sm font-medium transition-colors hover:text-primary">Server</Link>
+          <Link href="/settings" className="text-sm font-medium transition-colors hover:text-primary">Settings</Link>
         </div>
-        <div className="ml-auto flex items-center space-x-4">
-          <Input
-            type="search"
-            placeholder="Search..."
-            className="w-[200px] h-9"
-          />
-          <Avatar className="h-8 w-8">
-            <AvatarImage src="" alt="User" />
-            <AvatarFallback>UN</AvatarFallback>
-          </Avatar>
-        </div>
+
+        <Avatar className="h-10 w-10">
+          <AvatarImage src="" alt="User" />
+          <AvatarFallback></AvatarFallback>
+        </Avatar>
       </div>
-    </div>
+    </nav>
   );
 }
