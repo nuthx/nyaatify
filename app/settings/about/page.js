b25f17fb79ece6736f80cadf53832c225d932e78
NuthX <huermos@gmail.com>
2025-02-25 12:34:07 +0800

    feat(about): add database version

--- app/settings/about/page.js
+++ app/settings/about/page.js
@@ -1,5 +1,7 @@
 "use client";
 
+import useSWR from "swr"
+import { toast } from "sonner"
 import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import pkg from "@/package.json";
@@ -12,13 +14,36 @@ import {
 } from "@/components/ui/card"
 
 export default function Settings() {
+  const configApi = "/api/configs";
+
   const { t } = useTranslation();
   const currentVersion = pkg.version;
+
+  const { data, error, isLoading } = useSWR(configApi, async (url) => {
+    const response = await fetch(url);
+    const result = await response.json();
+    if (!response.ok) {
+      throw new Error(result.message);
+    }
+    return result.data;
+  });
 
   // Set page title
   useEffect(() => {
     document.title = `${t("st.metadata.about")} - Nyaatify`;
   }, [t]);
+
+  useEffect(() => {
+    if (error) {
+      toast.error(t("toast.failed.fetch_config"), {
+        description: error.message,
+      });
+    }
+  }, [error]);
+
+  if (isLoading) {
+    return <></>;
+  }
 
   return (
     <>
@@ -27,8 +52,9 @@ export default function Settings() {
           <CardTitle>{t("st.abt.version.title")}</CardTitle>
           <CardDescription>{t("st.abt.version.description")}</CardDescription>
         </CardHeader>
-        <CardContent>
-          <p className="text-sm text-muted-foreground">{t("glb.version")}: {currentVersion}</p>
+        <CardContent className="space-y-3">
+          <p className="text-sm text-muted-foreground">{t("st.abt.version.app_version")}: v{currentVersion}</p>
+          <p className="text-sm text-muted-foreground">{t("st.abt.version.db_version")}: v{data?.db_version}</p>
         </CardContent>
       </Card>
     </>
