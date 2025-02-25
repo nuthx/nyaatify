eb8b07709f5724f7f13858658728756cfa1cb3b4
NuthX <huermos@gmail.com>
2025-02-25 16:09:27 +0800

    feat(about): implement about page

--- app/settings/about/page.js
+++ app/settings/about/page.js
@@ -1,9 +1,10 @@
 "use client";
 
 import useSWR from "swr"
+import Image from "next/image";
 import { toast } from "sonner"
 import { useEffect } from "react";
-import { useTranslation } from "react-i18next";
+import { useTranslation, Trans } from "react-i18next";
 import pkg from "@/package.json";
 import {
   Card,
@@ -12,6 +13,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import { Badge } from "@/components/ui/badge";
 
 export default function Settings() {
   const configApi = "/api/configs";
@@ -47,14 +49,53 @@ export default function Settings() {
 
   return (
     <>
+      <div className="flex flex-col items-center mt-4 mb-12">
+        <Image src="/logo/nyaatify.svg" alt="Nyaatify Logo" width={72} height={72} />
+        <h1 className="text-2xl font-bold mt-8">Nyaatify</h1>
+        <p className="text-sm text-muted-foreground mt-2">{t("st.abt.description")}</p>
+        <div className="flex gap-2 mt-5">
+          <Badge variant="outline">{t("st.abt.app_version")}: v{currentVersion}</Badge>
+          <Badge variant="outline">{t("st.abt.db_version")}: v{data?.db_version}</Badge>
+        </div>
+        
+      </div>
+
       <Card>
         <CardHeader>
-          <CardTitle>{t("st.abt.version.title")}</CardTitle>
-          <CardDescription>{t("st.abt.version.description")}</CardDescription>
+          <CardTitle>{t("st.abt.project.title")}</CardTitle>
         </CardHeader>
-        <CardContent className="space-y-3">
-          <p className="text-sm text-muted-foreground">{t("st.abt.version.app_version")}: v{currentVersion}</p>
-          <p className="text-sm text-muted-foreground">{t("st.abt.version.db_version")}: v{data?.db_version}</p>
+        <CardContent className="space-y-6">
+          <div className="flex flex-col gap-2">
+            <h4 className="font-bold">{t("st.abt.project.author")}</h4>
+            <p className="text-sm text-muted-foreground">
+              <Trans i18nKey="st.abt.project.author_desc">
+                <a href="https://github.com/nuthx" target="_blank" className="underline" />
+              </Trans>
+            </p>
+          </div>
+          <div className="flex flex-col gap-2">
+            <h4 className="font-bold">{t("st.abt.project.license")}</h4>
+            <p className="text-sm text-muted-foreground">
+              <Trans i18nKey="st.abt.project.license_desc">
+                <a href="https://github.com/nuthx/nyaatify/blob/main/LICENSE" target="_blank" className="underline" />
+              </Trans>
+            </p>
+          </div>
+          <div className="flex flex-col gap-2">
+            <h4 className="font-bold">{t("st.abt.project.repository")}</h4>
+            <p className="text-sm text-muted-foreground">
+              <a href="https://github.com/nuthx/nyaatify" target="_blank" className="underline">Github</a>
+            </p>
+          </div>
+          <div className="flex flex-col gap-2">
+            <h4 className="font-bold">{t("st.abt.project.contribute")}</h4>
+            <p className="text-sm text-muted-foreground">
+              <Trans i18nKey="st.abt.project.contributor_desc">
+                <a href="https://github.com/nuthx/nyaatify/pulls" target="_blank" className="underline" />
+                <a href="https://github.com/nuthx/nyaatify/issues" target="_blank" className="underline" />
+              </Trans>
+            </p>
+          </div>
         </CardContent>
       </Card>
     </>
