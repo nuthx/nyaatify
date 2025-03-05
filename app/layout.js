27745bcfa7a99a1b79a80766e76af83493bf4565
NuthX <huermos@gmail.com>
2025-03-05 22:41:15 +0800

    refactor(app): move initialization to instrumentation

--- app/layout.js
+++ app/layout.js
@@ -2,9 +2,6 @@ import "./globals.css";
 import { AR_One_Sans } from "next/font/google"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { I18nWrapper } from "@/i18n/wrapper";
-import { initDb } from "@/lib/db";
-import { logger } from "@/lib/logger";
-import { startAllTasks } from "@/lib/schedule";
 import { Toaster } from "@/components/ui/sonner"
 import { NavBar } from "@/components/navbar";
 
@@ -14,17 +11,6 @@ const arOneSans = AR_One_Sans({
   display: "swap",
   fallback: ["-apple-system", "system-ui", "PingFang SC", "Hiragino Sans GB", "Microsoft Yahei", "Arial", "sans-serif"]
 })
-
-// Start RSS task and init DB when running in production
-if (process.env.NODE_ENV === "production") {
-  Promise.all([
-    initDb(),
-    startAllTasks()
-  ]).catch(logger.error);
-}
-
-// Init DB when running in development
-// initDb().catch(logger.error);
 
 export const metadata = {
   title: "Nyaatify",
