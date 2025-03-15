8d56d67b42ad28319b86919288552534129188ff
NuthX <huermos@gmail.com>
2025-03-15 12:11:38 +0800

    feat(db): split initConfigs and initUser, and log only if update

--- instrumentation.js
+++ instrumentation.js
@@ -1,9 +1,10 @@
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
-    const { initDb } = await import("@/lib/db")
+    const { initConfigs, initUser } = await import("@/lib/db")
     const { startAllTasks } = await import("@/lib/schedule")
 
-    await initDb();
+    await initConfigs();
+    await initUser();
     await startAllTasks();
   }
 }
