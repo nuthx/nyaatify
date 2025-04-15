2bedc5b6f1cac361559cfdc84478c9c27d932077
NuthX <huermos@gmail.com>
2025-04-15 18:16:10 +0800

    refactor(anime): improve responseive of RSS selection

--- app/anime/page.js
+++ app/anime/page.js
@@ -133,7 +133,7 @@ export default function Anime() {
   return (
     <div className="container mx-auto max-w-screen-xl flex flex-col py-8 space-y-6 px-6 md:px-10">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
-        <div className="flex justify-between items-center gap-4 mb-2 col-span-1 md:col-span-2">
+        <div className="flex flex-col md:flex-row justify-between md:items-center gap-4 mb-1 col-span-1 md:col-span-2">
           <div className="flex gap-4">
             {configData.downloaderStateDisplay === "1" && (
               torrentsData.downloaders.length === 0 ? (
@@ -147,19 +147,17 @@ export default function Anime() {
             <a className="text-sm text-muted-foreground">{t("anime.total")}: {animeData.count.total}</a>
           </div>
 
-          <div className="flex gap-4">
-            <Select defaultValue={selectedRss} onValueChange={handleRssChange}>
-              <SelectTrigger className="w-48">
-                <SelectValue />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="all">{t("glb.all")}</SelectItem>
-                {animeData.rss.list.map((rss, idx) => (
-                  <SelectItem key={idx} value={rss}>{rss}</SelectItem>
-                ))}
-              </SelectContent>
-            </Select>
-          </div>
+          <Select defaultValue={selectedRss} onValueChange={handleRssChange}>
+            <SelectTrigger className="w-full md:w-64">
+              <SelectValue />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="all">{t("glb.all")}</SelectItem>
+              {animeData.rss.list.map((rss, idx) => (
+                <SelectItem key={idx} value={rss}>{rss}</SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
         </div>
 
         {animeData.anime.length > 0 ? (
