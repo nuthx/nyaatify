6921a73be10d30a867872d6344ed97bc05486831
NuthX <huermos@gmail.com>
2025-04-16 09:47:50 +0800

    fix(settings): improve drag performance of sortable item component

--- app/settings/general/page.js
+++ app/settings/general/page.js
@@ -188,7 +188,7 @@ function SortableItem({ item }) {
   };
 
   return (
-    <div className="flex items-center justify-between h-10 pr-0.5 pl-3 py-2 w-full lg:w-72 transition-width duration-300 ease-in-out text-sm border rounded-md shadow-sm" ref={setNodeRef} style={style}>
+    <div className="flex items-center justify-between h-10 pr-0.5 pl-3 py-2 w-full lg:w-72 text-sm border rounded-md shadow-sm" ref={setNodeRef} style={style}>
       {item.name}
       <Button variant="ghost" size="icon" className="cursor-grab hover:cursor-grabbing hover:bg-transparent" {...attributes} {...listeners}>
         <GripVertical className="h-4 w-4" />
